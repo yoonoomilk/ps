5e9e8b0e0d58caee8e3d81acf8daaf73a8883a1a
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-05-27 18:30:37 +0900

    comb

--- template/etc/combination.cpp
+++ template/etc/combination.cpp
@@ -1,11 +1,11 @@
 using ll = long long;
 
-template <int sz, int MOD>
-  requires(sz >= 0, MOD >= 3)
+template <int sz>
 class combination {
+  const int MOD = 1e9 + 7;
   int fact[sz + 1], inv[sz + 1];
 
-  constexpr int _pow(ll n, int exp) {
+  int _pow(ll n, int exp) {
     ll tmp = 1;
     for(;exp;exp /= 2) {
       if (exp & 1) tmp = tmp * n % MOD;
@@ -15,14 +15,14 @@ class combination {
   }
 
 public:
-  constexpr combination() {
+  combination() {
     fact[0] = 1;
     for(int i = 1;i <= sz;i++) fact[i] = (ll)fact[i - 1] * i % MOD;
     inv[sz] = _pow(fact[sz], MOD - 2);
     for(int i = sz;i;i--) inv[i - 1] = (ll)inv[i] * i % MOD;
   }
 
-  ll operator() (int n, int r) const {
+  ll operator() (int n, int r) {
     return (ll)fact[n] * inv[r] % MOD * inv[n - r] % MOD;
   }
 };
