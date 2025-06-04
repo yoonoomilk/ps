48feeaca53578f2fd57cd10e1ab6ee839b586476
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-06-04 16:33:07 +0900

    rmq

--- template/range_query/const/const_rmq.cpp
+++ template/range_query/const/const_rmq.cpp
@@ -10,9 +10,10 @@ public:
     for(int i = 0;s != e;i++) arr[0][i] = *s++;
     for(int i = 1;i <= lg;i++) for(int j = 0;j + (1 << i - 1) < sz;j++) arr[i][j] = op(arr[i - 1][j], arr[i - 1][j + (1 << i - 1)]);
   }
+
   T query(int l, int r) {
-    int d = r - l + 1, dd = __lg(d);
-    int a = arr[dd][l], b = arr[dd][r - (1 << dd) + 1];
+    int d = __lg(r - l + 1);
+    int a = arr[d][l], b = arr[d][r - (1 << d) + 1];
     return op(a, b);
   }
 };
