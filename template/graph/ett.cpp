8ceffa607fc1a7550be9c2ba5dae29538a8342ed
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-06-09 15:35:17 +0900

    ett

--- template/graph/ett.cpp
+++ template/graph/ett.cpp
@@ -1,16 +1,12 @@
 vector<int> raw[100005];
-int ett[200005], dfsn;
-int dep[100005], in[100005];
+int arr[100005], arrn;
+int in[100005], out[100005];
 
-void dfs(int cur, int d) {
-  dep[cur] = d;
-  ett[dfsn] = cur;
-  in[cur] = dfsn++;
-  for(int i : raw[cur]) {
-    if(dep[i]) continue;
-    dfs(i, d + 1);
-    ett[dfsn++] = cur;
-  }
+void ett(int cur) {
+  in[cur] = ++arrn;
+  arr[arrn] = cur;
+  for(int i : raw[cur]) if(!in[i]) ett(i);
+  out[cur] = arrn;
 }
 
-dfs(1, 1);
+ett(1);
