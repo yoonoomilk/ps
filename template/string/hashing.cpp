77ddac4d525b8744aa73b6be99d6c7e141542c9a
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-07-03 17:16:27 +0900

    hmm

--- template/string/hashing.cpp
+++ template/string/hashing.cpp
@@ -8,8 +8,8 @@ public:
   hashing(I s, I e) : sz(distance(s, e)), h(sz + 1), b(sz + 1) {
     b[0] = 1;
     for(int i = 1;i <= sz;i++) {
-      b[i] = (ll)b[i - 1] * P % M;
-      h[i] = (h[i - 1] + (ll)b[i - 1] * *s++) % M;
+      h[i] = ((ll)h[i - 1] * P + *s++) % M;
+      b[i] = (ll)b[i-1] * P % M;
     }
   }
   ll operator() (int s, int e) {
