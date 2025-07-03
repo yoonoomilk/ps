75c3d9c8931e6931641e7a99769f0f58407ae7e8
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-07-03 17:24:32 +0900

    hmm

--- template/string/hashing.cpp
+++ template/string/hashing.cpp
@@ -1,20 +1,20 @@
 template <int P, int M>
 class hashing {
   const int sz;
-  vector<int> h, b;
+  vector<int> arr, _pow;
 
 public:
   template <typename I>
-  hashing(I s, I e) : sz(distance(s, e)), h(sz + 1), b(sz + 1) {
-    b[0] = 1;
+  hashing(I s, I e) : sz(distance(s, e)), arr(sz + 1), _pow(sz + 1) {
+    _pow[0] = 1;
     for(int i = 1;i <= sz;i++) {
-      h[i] = ((ll)h[i - 1] * P + *s++) % M;
-      b[i] = (ll)b[i-1] * P % M;
+      arr[i] = ((ll)arr[i - 1] * P + *s++) % M;
+      _pow[i] = (ll)_pow[i-1] * P % M;
     }
   }
   ll operator() (int s, int e) {
     s++; e++;
-    return (h[e] - (ll)h[s - 1] * b[e - s + 1] + M) % M;
+    return (arr[e] - (ll)arr[s - 1] * _pow[e - s + 1] + M) % M;
   }
   ll operator() () {
     return (*this)(0, sz - 1);
