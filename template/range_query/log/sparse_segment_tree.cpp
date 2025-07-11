511d68f2ee85334026cbb78d8216d17ea4ee05af
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-07-11 21:06:32 +0900

    Create sparse_segment_tree.cpp

---
+++ template/range_query/log/sparse_segment_tree.cpp
@@ -0,0 +1,72 @@
+# TODO
+
+template <typename T, typename L, typename Merge, typename Update, typename Composition>
+class sparse_segment_tree {
+  const int sz;
+  const T raw;
+  const L lazy_raw;
+  struct node {
+    int l, r;
+    T v;
+    L lazy;
+    node() : l(-1), r(-1) {
+      v = raw;
+      lazy = lazy_raw;
+    }
+  };
+  vector<node> tree;
+  Merge op;
+  Update upd;
+  Composition comp;
+
+  int append() {
+    tree.push_back(node());
+    return tree.size() - 1;
+  }
+
+  void push(int cur, int s, int e) {
+    
+  }
+
+  void update(int cur, int s, int e, int i, T& v) {
+    if(s == e) {
+      tree[cur].v = v;
+      return;
+    }
+    int m = (s + e) / 2;
+    if(i <= m) {
+      if(tree[cur].l == -1) tree[cur].l = append();
+      update(tree[cur].l, s, m, i, v);
+    } else {
+      if(tree[cur].r == -1) tree[cur].r = append();
+      update(tree[cur].r, m + 1, e, i, v);
+    }
+    tree[cur].v = op(
+      tree[cur].l != -1 ? tree[tree[cur].l].v : raw,
+      tree[cur].r != -1 ? tree[tree[cur].r].v : raw
+    );
+  }
+
+  T query(int cur, int s, int e, int l, int r) {
+    if(cur == -1 || r < s || e < l) return raw;
+    if(l <= s && e <= r) return tree[cur].v;
+    int m = (s + e) / 2;
+    return op(
+      query(tree[cur].l, s, m, l, r),
+      query(tree[cur].r, m + 1, e, l, r)
+    );
+  }
+
+public:
+  sparse_segment_tree(int mx, T raw = T()) : sz(mx + 1), raw(raw) {
+    append();
+  }
+
+  void update(int i, T v) {
+    update(0, 0, sz - 1, i, v);
+  }
+
+  T query(int l, int r) {
+    return query(0, 0, sz - 1, l, r);
+  }
+};
