fbe8833a69ef092b83dd7101cb5a46740af726ac
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-07-18 13:39:45 +0900

    mst

--- template/range_query/log/fenwick_tree/merge_sort_tree-fenwick_tree.cpp
+++ template/range_query/log/fenwick_tree/merge_sort_tree-fenwick_tree.cpp
@@ -7,7 +7,7 @@ class merge_sort_tree {
 public:
   template <typename I>
   merge_sort_tree(I s, I e) : sz(distance(s, e)), tree(sz + 1) {
-    for(int i = 0;i < sz;i++) for(int j = i + 1;j <= sz;j += j & -j) tree[j].push_back(*s++);
+    for(int i = 0;i < sz;i++, s++) for(int j = i + 1;j <= sz;j += j & -j) tree[j].push_back(*s);
     for(int i = 1;i <= sz;i++) sort(tree[i].begin(), tree[i].end());
   }
 
