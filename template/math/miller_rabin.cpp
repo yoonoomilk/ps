a5b29c0eac7b260d7465a2925f9dc09417002c2c
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-04 19:00:01 +0900

    hmm

---
+++ template/math/miller_rabin.cpp
@@ -0,0 +1,29 @@
+ll _pow(ll n, ll exp, ll mod) {
+  __int128 tmp = 1;
+  for(n %= mod;exp;exp /= 2) {
+    if(exp & 1) tmp = tmp * n % mod;
+    n = (n * n) % mod;
+  }
+  return tmp;
+}
+
+bool miller_rabin(ll n) {
+  if(n == 1) return false;
+  if(n == 2) return true;
+  for(ll a : {2, 7, 61}) if(a % n) {
+    for(ll k = n - 1;;k /= 2) {
+      ll tmp = _pow(a, k, n);
+      if(tmp == n - 1) break;
+      if(k % 2) {
+        if(tmp == 1) break;
+        else return false;
+      }
+    }
+  }
+  return true;
+}
+
+/*
+n <= 2^32 -> {2, 7, 61}
+n <= 2^64 -> {2, 325, 9375, 28178, 450775, 9780504, 1795265022}
+*/
--- template/math/pow.cpp
+++ template/math/pow.cpp
@@ -1,7 +1,7 @@
 ll _pow(ll n, ll exp, ll mod) {
   ll tmp = 1;
   for(;exp;exp /= 2) {
-    if (exp & 1) tmp = tmp * n % mod;
+    if(exp & 1) tmp = tmp * n % mod;
     n = (n * n) % mod;
   }
   return tmp;
