2d2114cdec7ddb81d51a14331fcc7f0a36922a60
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-04 18:10:34 +0900

    hmm

--- template/string/kmp.cpp
+++ template/string/kmp.cpp
@@ -2,7 +2,7 @@ vector<int> match;
 
 vector<int> pidx(const string& p) {
   vector<int> pi(p.size());
-  for(int i = 1, j = 0;i < m;i++) {
+  for(int i = 1, j = 0;i < p.size();i++) {
     while(j && p[i] != p[j]) j = pi[j - 1];
     if(p[i] == p[j]) pi[i] = ++j;
   }
@@ -11,7 +11,7 @@ vector<int> pidx(const string& p) {
 
 vector<int> kmp(string& s, string>& p, vector<int>& pi) {
   vector<int> match;
-  for(int i = 0, j = 0;i < n;i++) {
+  for(int i = 0, j = 0;i < s.size();i++) {
     while(j && s[i] != p[j]) j = pi[j - 1];
     if(s[i] != p[j]) continue;
     if(j == m - 1) {
