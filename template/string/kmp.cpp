d5cc6f3a285d81b901ba16bbdcf87538f0539dfa
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-05 16:44:43 +0900

    kmp

--- template/string/kmp.cpp
+++ template/string/kmp.cpp
@@ -1,5 +1,3 @@
-vector<int> match;
-
 vector<int> pidx(string_view p) {
   vector<int> pi(p.size());
   for(int i = 1, j = 0;i < p.size();i++) {
@@ -14,10 +12,36 @@ vector<int> kmp(string_view s, string_view p) {
   for(int i = 0, j = 0;i < s.size();i++) {
     while(j && s[i] != p[j]) j = pi[j - 1];
     if(s[i] != p[j]) continue;
-    if(j == m - 1) {
+    if(j == p.size() - 1) {
       match.push_back(i - j);
       j = pi[j];
     } else j++;
   }
   return match;
 }
+
+class kmp {
+  const int ssz, psz;
+  vector<int> pi, match;
+
+public:
+  template <typename I>
+  kmp(I s, I se, I p, I pe) : ssz(distance(s, se)), psz(distance(p, pe)), pi(psz) {
+    for(int i = 1, j = 0;i < psz;i++) {
+      while(j && p[i] != p[j]) j = pi[j - 1];
+      if(p[i] == p[j]) pi[i] = ++j;
+    }
+    for(int i = 0, j = 0;i < ssz;i++) {
+      while(j && s[i] != p[j]) j = pi[j - 1];
+      if(s[i] != p[j]) continue;
+      if(j == psz - 1) {
+        match.push_back(i - j);
+        j = pi[j];
+      } else j++;
+    }
+  }
+
+  vector<int>& operator() {
+    return match;
+  }
+};
