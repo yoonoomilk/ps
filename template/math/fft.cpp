646459fecbde9c21e2c59feb26218f4ac19f76fa
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-08 10:56:51 +0900

    fft

--- template/math/fft.cpp
+++ template/math/fft.cpp
@@ -1,42 +1,43 @@
-const double PI = acos(-1);
 using base = complex<double>;
 
-void fft(vector<base>& v, bool inv) {
-  for(int i = 1, j = 0;i < v.size();i++) {
-    int bit = v.size() / 2;
+template<typename I>
+requires same_as<typename iterator_traits<I>::value_type, base>
+void fft(I s, I e, bool inv = false) {
+  int sz = distance(s, e);
+  for(int i = 1, j = 0;i < sz;i++) {
+    int bit = sz / 2;
     while(j >= bit) {
       j -= bit;
       bit /= 2;
     }
     j += bit;
-    if(i < j) swap(v[i], v[j]);
+    if(i < j) swap(s[i], s[j]);
   }
-  for(int k = 1;k < v.size();k *= 2) {
-    double angle = inv ? PI / k : -PI / k;
+  for(int k = 1;k < sz;k *= 2) {
+    double angle = inv ? numbers::pi / k : -numbers::pi / k;
     base w(cos(angle), sin(angle));
-    for(int i = 0;i < v.size();i += k*2) {
+    for(int i = 0;i < sz;i += k * 2) {
       base z(1, 0);
       for(int j = 0;j < k;j++) {
-        base even = v[i+j], odd = v[i+j+k];
-        v[i+j] = even + z * odd;
-        v[i+j+k] = even - z * odd;
+        base even = s[i + j], odd = s[i + j + k];
+        s[i + j] = even + z * odd;
+        s[i + j + k] = even - z * odd;
         z *= w;
       }
     }
   }
-
-  if(inv) for(base& i : v) i /= v.size();
+  if(inv) while(s != e) *s++ /= sz;
 }
 
 vector<base> mul(vector<base> a, vector<base> b) {
   int sz = 1;
   while(sz < a.size() + b.size()) sz *= 2;
 
-  a.resize(sz); fft(a, false);
-  b.resize(sz); fft(b, false);
+  a.resize(sz); fft(a.begin(), a.end());
+  b.resize(sz); fft(b.begin(), b.end());
 
   for(int i = 0;i < sz;i++) a[i] *= b[i];
-  fft(a, true);
+  fft(a.begin(), a.end(), true);
 
   return a;
 }
