68380c9019fa64e8da0b5067ccfbb86e14b204ea
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-09 23:04:06 +0900

    eraseable pq

---
+++ template/etc/eraseable_pq.cpp
@@ -0,0 +1,17 @@
+template <typename T, typename comp = greater<T>>
+class eraseable_pq {
+  priority_queue<T, vector<T>, comp> pq, del;
+
+public:
+  eraseable_pq() {}
+  size_t size() { return pq.size() - del.size(); };
+  void insert(T v) { pq.push(v); }
+  void erase(T v) {
+    del.push(v);
+    while(del.size() && pq.top() == del.top()) {
+      pq.pop();
+      del.pop();
+    }
+  }
+  T top() { return pq.top(); }
+}
