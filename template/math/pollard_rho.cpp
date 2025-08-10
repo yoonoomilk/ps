917ed7a89fe1cb7c1759a51f02852f515e9079ba
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-10 13:26:55 +0900

    pollard rho

--- template/math/pollard_rho.cpp
+++ template/math/pollard_rho.cpp
@@ -1,25 +1,23 @@
 vector<ll> pollard_rho(ll n) {
+  srand(time(nullptr));
   vector<ll> tmp;
   function<void(ll)> dfs = [&](ll n) -> void {
     if(n == 1) return;
-    if(n % 2 == 0) {
-      tmp.push_back(2);
-      dfs(n / 2);
-      return;
-    }
     if(miller_rabin(n)) {
       tmp.push_back(n);
       return;
     }
-    ll g = 1;
-    for(ll a = rand() % (n - 2) + 2, b = a, c = rand() % 20 + 1;g == 1;g = __gcd(abs(a - b), n)) {
-      a = (_pow(a, 2, n) + c) % n;
-      b = (_pow(b, 2, n) + c) % n;
-      b = (_pow(b, 2, n) + c) % n;
+    ll a = rand() % (n - 2) + 2, b = a, c = rand() % 20 + 1, g = 1;
+    while(g == 1) {
+      a = ((__int128)a * a + c) % n;
+      b = ((__int128)b * b + c) % n;
+      b = ((__int128)b * b + c) % n;
+      g = __gcd(abs(a - b), n);
     }
     dfs(g);
     dfs(n / g);
   };
+  for(;n % 2 == 0;n /= 2) tmp.push_back(2);
   dfs(n);
   sort(tmp.begin(), tmp.end());
   return tmp;
