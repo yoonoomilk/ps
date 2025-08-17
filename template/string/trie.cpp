7d04168b9f9e4308476fa07bc6f7b35ff01b0866
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-17 16:40:57 +0900

    map trie

--- template/string/trie.cpp
+++ template/string/trie.cpp
@@ -24,3 +24,31 @@ public:
     cur->have = true;
   }
 };
+
+class trie {
+public:
+  map<char, trie*> arr;
+  bool have = false;
+
+  trie() { }
+
+  trie* child(char c) {
+    if(auto it = arr.find(c);it != arr.end()) return it->second;
+    return nullptr;
+  }
+
+  template <typename I>
+  void insert(I s, I e) {
+    trie* cur = this;
+    for(;s != e;s++) {
+      trie* nxt = cur->child(*s);
+      if(nxt == nullptr) {
+        trie* tmp = new trie();
+        cur->arr[*s] = tmp;
+        nxt = tmp;
+      }
+      cur = nxt;
+    }
+    cur->have = true;
+  }
+};
