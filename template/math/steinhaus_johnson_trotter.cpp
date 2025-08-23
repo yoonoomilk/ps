3f3df5779d6c45bf24ceadbf85510d442e5f9b79
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-23 23:13:12 +0900

    steinhaus_johnson_trotter

---
+++ template/math/steinhaus_johnson_trotter.cpp
@@ -0,0 +1,24 @@
+template <typename I>
+class steinhaus_johnson_trotter {
+  const int n;
+  I s, e;
+  vector<char> dir;
+
+public:
+  steinhaus_johnson_trotter(I s, I e) : n(distance(s, e)), s(s), e(e), dir(n, -1) {}
+
+  bool next() {
+    int n = distance(s, e);
+    int tmp = -1;
+    for(int i = 0;i < n;i++) {
+      int nxt = i + dir[i];
+      if(nxt < 0 || nxt >= n) continue;
+      if(s[i] > s[nxt] && (tmp == -1 || s[i] > s[tmp])) tmp = i;
+    }
+    if(tmp == -1) return false;
+    int nxt = tmp + dir[tmp];
+    for(int i = 0;i < n;i++) if(s[i] > s[nxt]) dir[i] *= -1;
+  }
+}
+
+//https://en.wikipedia.org/wiki/Steinhaus%E2%80%93Johnson%E2%80%93Trotter_algorithm
