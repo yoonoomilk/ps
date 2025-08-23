e63e29bd771e52516dea155fd419b912e1ee70ca
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-23 13:54:59 +0900

    miller rabin

--- template/math/miller_rabin.cpp
+++ template/math/miller_rabin.cpp
@@ -10,7 +10,7 @@ ll _pow(__int128 n, ll exp, ll mod) {
 bool miller_rabin(ll n) {
   if(n == 1) return false;
   if(n == 2) return true;
-  for(ll a : {2, 7, 61}) if(a % n) {
+  for(ll a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) if(a % n) {
     for(ll k = n - 1;;k /= 2) {
       ll tmp = _pow(a, k, n);
       if(tmp == n - 1) break;
