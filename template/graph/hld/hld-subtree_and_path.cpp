c399c4f4c072acc792157ed07cfe5aac63867bc0
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-29 10:18:46 +0900

    hld

--- template/graph/hld/hld-subtree_and_path.cpp
+++ template/graph/hld/hld-subtree_and_path.cpp
@@ -2,31 +2,19 @@ template <typename T, typename L, typename Merge, typename Update, typename Comp
 class heavy_light_decomposition {
   const int sz;
   const T raw;
-  vector<int> weight, dep, pa, top, in;
+  const L lazy_raw;
+  vector<int> weight, dep, pa, top, in, out;
   vector<vector<int>> edges;
   lazy_segment_tree<T, L, Merge, Update, Composition> seg;
   Merge op;
-  struct lca_op {
-    pii operator() (pii a, pii b) {
-      return min(a, b);
-    }
-  };
-  segment_tree<pii, lca_op> lca;
-
-  T query(int a, int b) {
-    T s = raw;
-    for(;top[a] != top[b];a = pa[top[a]]) {
-      if(dep[top[a]] < dep[top[b]]) swap(a, b);
-      s = op(s, seg(in[top[a]], in[a]));
-    }
-    if(dep[a] > dep[b]) swap(a, b);
-    return op(s, seg(in[a] + 1, in[b]));
-  }
 
 public:
-  heavy_light_decomposition(int n, T raw) : sz(n + 1), raw(raw),
-    weight(sz), dep(sz), pa(sz), top(sz), in(sz),
-    edges(sz), seg(sz * 2, raw), lca(sz * 2, {sz, sz}) {}
+  heavy_light_decomposition(int n, T raw, L lazy_raw) : sz(n + 1), raw(raw), lazy_raw(lazy_raw),
+    weight(sz), dep(sz), pa(sz), top(sz), in(sz), out(sz),
+    edges(sz), seg(sz, raw, lazy_raw) {
+    for(int i = 0;i < sz;i++) seg.set(i, {0, 1});
+    seg.init();
+  }
 
   void add(int a, int b) {
     edges[a].push_back(b);
@@ -46,12 +34,12 @@ public:
     dfs(1, 0);
     int ettn = 0;
     function <void(int, int)> ett = [&](int cur, int bef) {
-      lca.update(in[cur] = ettn++, {dep[cur], cur});
+      in[cur] = ettn++;
       for(int i : edges[cur]) if(i != bef) {
         top[i] = (i == edges[cur][0] ? top[cur] : i);
         ett(i, cur);
-        lca.update(ettn++, {dep[cur], cur});
       }
+      out[cur] = ettn - 1;
     };
     top[1] = 1;
     ett(1, 0);
@@ -69,8 +57,16 @@ public:
     seg.update(in[a], in[b], v);
   }
 
+  T operator() (int a) {
+    return seg(in[a], out[a]);
+  }
   T operator() (int a, int b) {
-    int l = lca(min(in[a], in[b]), max(in[a], in[b])).second;
-    return op(query(a, l), query(b, l));
+    T s = raw;
+    for(;top[a] != top[b];a = pa[top[a]]) {
+      if(dep[top[a]] < dep[top[b]]) swap(a, b);
+      s = op(s, seg(in[top[a]], in[a]));
+    }
+    if(dep[a] > dep[b]) swap(a, b);
+    return op(s, seg(in[a], in[b]));
   }
 };
