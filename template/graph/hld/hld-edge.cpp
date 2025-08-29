8702a22c1fc4e9a6a977d8414f3931a02097c393
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-08-29 09:42:35 +0900

    hld

--- template/graph/hld/hld-edge.cpp
+++ template/graph/hld/hld-edge.cpp
@@ -6,27 +6,11 @@ class heavy_light_decomposition {
   vector<vector<int>> edges;
   segment_tree<T, Merge> seg;
   Merge op;
-  struct lca_op {
-    pii operator() (pii a, pii b) {
-      return min(a, b);
-    }
-  };
-  segment_tree<pii, lca_op> lca;
-
-  T query(int a, int b) {
-    T s = raw;
-    for(;top[a] != top[b];a = pa[top[a]]) {
-      if(dep[top[a]] < dep[top[b]]) swap(a, b);
-      s = op(s, seg(in[top[a]], in[a]));
-    }
-    if(dep[a] > dep[b]) swap(a, b);
-    return op(s, seg(in[a] + 1, in[b]));
-  }
 
 public:
   heavy_light_decomposition(int n, T raw) : sz(n + 1), raw(raw),
     weight(sz), dep(sz), pa(sz), top(sz), in(sz),
-    edges(sz), seg(sz * 2, raw), lca(sz * 2, {sz, sz}) {}
+    edges(sz), seg(sz, raw) {}
 
   void add(int a, int b) {
     edges[a].push_back(b);
@@ -46,11 +30,10 @@ public:
     dfs(1, 0);
     int ettn = 0;
     function <void(int, int)> ett = [&](int cur, int bef) {
-      lca.update(in[cur] = ettn++, {dep[cur], cur});
+      in[cur] = ettn++;
       for(int i : edges[cur]) if(i != bef) {
         top[i] = (i == edges[cur][0] ? top[cur] : i);
         ett(i, cur);
-        lca.update(ettn++, {dep[cur], cur});
       }
     };
     top[1] = 1;
@@ -63,7 +46,12 @@ public:
   }
 
   T operator() (int a, int b) {
-    int l = lca(min(in[a], in[b]), max(in[a], in[b])).second;
-    return op(query(a, l), query(b, l));
+    T s = raw;
+    for(;top[a] != top[b];a = pa[top[a]]) {
+      if(dep[top[a]] < dep[top[b]]) swap(a, b);
+      s = op(s, seg(in[top[a]], in[a]));
+    }
+    if(dep[a] > dep[b]) swap(a, b);
+    return op(s, seg(in[a] + 1, in[b]));
   }
 };
