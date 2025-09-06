757e024c392569e2fbe57a2f1b53407a21653349
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-09-06 21:17:08 +0900

    hmm

--- template/graph/scc.cpp
+++ template/graph/scc.cpp
@@ -1,16 +1,19 @@
+// dag
 class strongly_connected_component {
   const int sz;
-  vector<vector<int>> edges, _scc, _dag;
+  vector<vector<int>> edges;
   vector<int> idx;
 
 public:
+  vector<vector<int>> scc, dag;
+
   strongly_connected_component(int n) : sz(n + 1), edges(sz), idx(sz, -1) {}
 
   void add(int a, int b) {
     edges[a].push_back(b);
   }
 
-  void operator() () {
+  void init() {
     vector<int> pa(sz, -1);
     vector<bool> finished(sz);
     stack<int> st;
@@ -23,29 +26,72 @@ public:
         else if(!finished[i]) bef = min(bef, pa[i]);
       }
       if(bef == pa[cur]) {
-        _scc.push_back({});
+        scc.push_back({});
         while(1) {
           int i = st.top();
           st.pop();
-          _scc.back().push_back(i);
+          scc.back().push_back(i);
           finished[i] = true;
-          idx[i] = _scc.size() - 1;
+          idx[i] = scc.size() - 1;
           if(cur == i) break;
         }
-        sort(_scc.back().begin(), _scc.back().end());
+        sort(scc.back().begin(), scc.back().end());
       }
       return bef;
     };
     for(int i = 1;i < sz;i++) if(pa[i] == -1) dfs(i);
-    _dag.resize(_scc.size());
-    for(int i = 1;i < sz;i++) for(int j : edges[i]) if(idx[i] != idx[j]) _dag[idx[i]].push_back(idx[j]);
-    for(auto& i : _dag) {
+    dag.resize(scc.size());
+    for(int i = 1;i < sz;i++) for(int j : edges[i]) if(idx[i] != idx[j]) dag[idx[i]].push_back(idx[j]);
+    for(auto& i : dag) {
       sort(i.begin(), i.end());
       i.erase(unique(i.begin(), i.end()), i.end());
     }
   }
 
-  const vector<vector<int>>& scc() { return _scc; }
-  const vector<vector<int>>& dag() { return _dag; }
+  int operator[] (int i) { return idx[i]; }
+};
+
+// short ver
+class strongly_connected_component {
+  const int sz;
+  vector<vector<int>> edges;
+  vector<int> idx;
+  int scc = 0;
+
+public:
+
+  strongly_connected_component(int n) : sz(n + 1), edges(sz), idx(sz, -1) {}
+
+  void add(int a, int b) {
+    edges[a].push_back(b);
+  }
+
+  void init() {
+    vector<int> pa(sz, -1);
+    vector<bool> finished(sz);
+    stack<int> st;
+    int cnt = 0;
+    function<int(int)> dfs = [&](int cur) {
+      st.push(cur);
+      int bef = pa[cur] = ++cnt;
+      for(int i : edges[cur]) {
+        if(pa[i] == -1) bef = min(bef, dfs(i));
+        else if(!finished[i]) bef = min(bef, pa[i]);
+      }
+      if(bef == pa[cur]) {
+        scc++;
+        while(1) {
+          int i = st.top();
+          st.pop();
+          finished[i] = true;
+          idx[i] = scc - 1;
+          if(cur == i) break;
+        }
+      }
+      return bef;
+    };
+    for(int i = 1;i < sz;i++) if(pa[i] == -1) dfs(i);
+  }
+
   int operator[] (int i) { return idx[i]; }
 };
