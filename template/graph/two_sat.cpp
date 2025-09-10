5accac82cb2e8b8f0cc02a4716094d313f34ca0a
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-09-10 18:31:29 +0900

    sat

--- template/graph/two_sat.cpp
+++ template/graph/two_sat.cpp
@@ -17,7 +17,7 @@ public:
 
   bool operator() () {
     bool flag = true;
-    scc();
+    scc.init();
     for(int i = 1;i < sz && flag;i++) flag &= scc[i * 2] != scc[i * 2 + 1];
     if(flag) {
       vector<pair<int, int>> arr(sz * 2 - 2);
@@ -35,4 +35,29 @@ public:
   }
 
   bool operator[] (int i) { return ans[i * 2]; }
+};
+
+//short ver
+class two_sat {
+  const int sz;
+  strongly_connected_component scc;
+
+public:
+  two_sat(int n) : sz(n + 1), scc(sz * 2 + 1) {}
+
+  void add(int a, int b) {
+    if(a > 0) a = a * 2;
+    else a = -a * 2 + 1;
+    if(b > 0) b = b * 2;
+    else b = -b * 2 + 1;
+    scc.add(a ^ 1, b);
+    scc.add(b ^ 1, a);
+  }
+
+  bool operator() () {
+    bool flag = true;
+    scc.init();
+    for(int i = 1;i < sz && flag;i++) flag &= scc[i * 2] != scc[i * 2 + 1];
+    return flag;
+  }
 };
