dc9025ee909db0ad9de986e1255c6b037ba02479
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-09-14 23:25:48 +0900

    rotating_calipers

--- template/geometry/rotating_calipers.cpp
+++ template/geometry/rotating_calipers.cpp
@@ -10,3 +10,16 @@ pair<pt, pt> rotating_calipers(polygon& poly) {
   }
   return tmp.second;
 };
+
+ll rotating_calipers(polygon& poly) {
+  int sz = poly.size();
+  ll tmp = dist2(poly[0] - poly[1]);
+  for(int i = 0, j = 0;i < sz;i++) {
+    while(j < sz * 2 && ccw(poly[i], poly[(i + 1) % sz], poly[(i + 1) % sz] + poly[(j + 1) % sz] - poly[j % sz]) < 0) {
+      tmp = max(tmp, dist2(poly[i] - poly[j % sz]));
+      j = (j + 1) % sz;
+    }
+    tmp = max(tmp, dist2(poly[i] - poly[j % sz]));
+  }
+  return tmp;
+};
