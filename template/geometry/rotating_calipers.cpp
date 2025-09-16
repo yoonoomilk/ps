f5c1817cbea3f1d16717ba5f3ce4e2267ae6b916
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-09-16 17:39:33 +0900

    rotating_calipers

--- template/geometry/rotating_calipers.cpp
+++ template/geometry/rotating_calipers.cpp
@@ -2,7 +2,7 @@ pair<pt, pt> rotating_calipers(polygon& poly) {
   int sz = poly.size();
   pair<ll, pair<pt, pt>> tmp = {dist2(poly[0] - poly[1]), {poly[0], poly[1]}};
   for(int i = 0, j = 0;i < sz;i++) {
-    while(j < sz * 2 && ccw(poly[i], poly[(i + 1) % sz], poly[(i + 1) % sz] + poly[(j + 1) % sz] - poly[j % sz]) < 0) {
+    while(j < sz * 2 && ccw(poly[i], poly[(i + 1) % sz], poly[(i + 1) % sz] + poly[(j + 1) % sz] - poly[j % sz]) > 0) {
       tmp = max(tmp, {dist2(poly[i] - poly[j % sz]), {poly[i], poly[j % sz]}});
       j = (j + 1) % sz;
     }
@@ -15,7 +15,7 @@ ll rotating_calipers(polygon& poly) {
   int sz = poly.size();
   ll tmp = dist2(poly[0] - poly[1]);
   for(int i = 0, j = 0;i < sz;i++) {
-    while(j < sz * 2 && ccw(poly[i], poly[(i + 1) % sz], poly[(i + 1) % sz] + poly[(j + 1) % sz] - poly[j % sz]) < 0) {
+    while(j < sz * 2 && ccw(poly[i], poly[(i + 1) % sz], poly[(i + 1) % sz] + poly[(j + 1) % sz] - poly[j % sz]) > 0) {
       tmp = max(tmp, dist2(poly[i] - poly[j % sz]));
       j = (j + 1) % sz;
     }
