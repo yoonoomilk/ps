154d9b8457a3d5d14c56710711616393718d062a
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-09-23 16:39:53 +0900

    dynamic seg with 64bit index

--- template/range_query/segment_tree/dynamic_segment_tree.cpp
+++ template/range_query/segment_tree/dynamic_segment_tree.cpp
@@ -1,6 +1,6 @@
 template <typename T, typename Merge>
 class dynamic_segment_tree {
-  const int sz;
+  const ll sz;
   const T raw;
   struct node {
     int l, r;
@@ -15,12 +15,12 @@ class dynamic_segment_tree {
     return tree.size() - 1;
   }
 
-  void update(int cur, int s, int e, int i, T& v) {
+  void update(int cur, ll s, ll e, ll i, T& v) {
     if(s == e) {
-      tree[cur].v = v;
+      tree[cur].v += v;
       return;
     }
-    int m = (s + e) / 2;
+    ll m = (s + e) / 2;
     if(tree[cur].l == -1) tree[cur].l = append();
     if(tree[cur].r == -1) tree[cur].r = append();
     update(tree[cur].l, s, m, i, v);
@@ -28,10 +28,10 @@ class dynamic_segment_tree {
     tree[cur].v = op(tree[tree[cur].l].v, tree[tree[cur].r].v);
   }
 
-  T query(int cur, int s, int e, int l, int r) {
+  T query(int cur, ll s, ll e, ll l, ll r) {
     if(cur == -1 || r < s || e < l) return raw;
     if(l <= s && e <= r) return tree[cur].v;
-    int m = (s + e) / 2;
+    ll m = (s + e) / 2;
     return op(
       query(tree[cur].l, s, m, l, r),
       query(tree[cur].r, m + 1, e, l, r)
@@ -39,15 +39,15 @@ class dynamic_segment_tree {
   }
 
 public:
-  dynamic_segment_tree(int mx, T raw = T()) : sz(mx + 1), raw(raw) {
+  dynamic_segment_tree(ll mx, T raw = T()) : sz(mx + 1), raw(raw) {
     append();
   }
 
-  void update(int i, T v) {
+  void update(ll i, T v) {
     update(0, 0, sz - 1, i, v);
   }
 
-  T query(int l, int r) {
+  T query(ll l, ll r) {
     return query(0, 0, sz - 1, l, r);
   }
 };
