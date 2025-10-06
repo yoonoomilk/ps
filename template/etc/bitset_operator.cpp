8d361cf02eeed52595a919ed56c103f9b1f16cb2
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-10-06 19:55:56 +0900

    some tricks

---
+++ template/etc/bitset_operator.cpp
@@ -0,0 +1,39 @@
+// https://gist.github.com/cgiosy/a441de545c9e96b1d7b02cc7a00561f9
+
+#define private public
+#include <bitset>
+#undef private
+#include <x86intrin.h>
+
+template<size_t _Nw> void _M_do_sub(_Base_bitset<_Nw>& A, const _Base_bitset<_Nw>& B) {
+  for(int i = 0, c = 0; i < _Nw; i++)
+    c = _subborrow_u64(c, A._M_w[i], B._M_w[i], (unsigned long long*) & A._M_w[i]);
+}
+template<> void _M_do_sub(_Base_bitset<1>& A, const _Base_bitset<1>& B) {
+  A._M_w -= B._M_w;
+}
+template<size_t _Nb> bitset<_Nb>& operator-=(bitset<_Nb>& A, const bitset<_Nb>& B) {
+  _M_do_sub(A, B);
+  return A;
+}
+template<size_t _Nb> inline bitset<_Nb> operator-(const bitset<_Nb>& A, const bitset<_Nb>& B) {
+  bitset<_Nb> C(A);
+  return C -= B;
+}
+
+
+template<size_t _Nw> void _M_do_add(_Base_bitset<_Nw>& A, const _Base_bitset<_Nw>& B) {
+  for(int i = 0, c = 0; i < _Nw; i++)
+    c = _addcarry_u64(c, A._M_w[i], B._M_w[i], (unsigned long long*) & A._M_w[i]);
+}
+template<> void _M_do_add(_Base_bitset<1>& A, const _Base_bitset<1>& B) {
+  A._M_w += B._M_w;
+}
+template<size_t _Nb> bitset<_Nb>& operator+=(bitset<_Nb>& A, const bitset<_Nb>& B) {
+  _M_do_add(A, B);
+  return A;
+}
+template<size_t _Nb> inline bitset<_Nb> operator+(const bitset<_Nb>& A, const bitset<_Nb>& B) {
+  bitset<_Nb> C(A);
+  return C += B;
+}
--- template/etc/eraseable_pq.cpp
+++ template/etc/eraseable_pq.cpp
@@ -1,17 +1,18 @@
-template <typename T, typename comp = greater<T>>
+template <typename T, typename comp = less<T>>
 class eraseable_pq {
   priority_queue<T, vector<T>, comp> pq, del;
 
-public:
-  eraseable_pq() {}
-  size_t size() { return pq.size() - del.size(); };
-  void insert(T v) { pq.push(v); }
-  void erase(T v) { del.push(v); }
-  T top() {
+  void flush() {
     while(del.size() && pq.top() == del.top()) {
       pq.pop();
       del.pop();
     }
-    return pq.top();
   }
+public:
+  eraseable_pq() {}
+  size_t size() { return pq.size() - del.size(); };
+  void insert(T v) { pq.push(v); flush(); }
+  void erase(T v) { del.push(v); flush(); }
+  void pop() { del.pop(); flush(); }
+  const T& top() { return pq.top(); }
 };
