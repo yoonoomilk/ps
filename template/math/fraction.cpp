72ea84395d3898a1f9320743c4cc2e4653f2f068
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-10-26 15:38:27 +0900

    fraction typo

--- template/math/fraction.cpp
+++ template/math/fraction.cpp
@@ -1,24 +1,18 @@
 class fraction {
   ll n, d;
 public:
-  fraction(ll a = 0, ll b = 1) : n(a), d(b) {
-    assert(d != 0);
-    ll g = gcd(a, b);
-    n /= g;
-    d /= g;
-  }
+  fraction(ll a = 0, ll b = 1) : n(a / gcd(a, b)), d(b / gcd(a, b)) {}
+  fraction operator+ (const fraction v) const { return fraction(n * v.d + d * v.n, d * v.d); }
+  fraction operator+= (const fraction v) { return *this = *this + v; }
+  fraction operator- (const fraction v) const { return fraction(n * v.d - d * v.n, d * v.d); }
+  fraction operator-= (const fraction v) { return *this = *this - v; }
+  fraction operator* (const fraction v) const { return fraction(n * v.n, d * v.d); }
+  fraction operator*= (const fraction v) { return *this = *this * v; }
+  fraction operator/ (const fraction v) const { return fraction(n * v.d, d * v.n); }
+  fraction operator/= (const fraction v) { return *this = *this / v; }
+  fraction operator- () const { return fraction(-n, d); }
 
-  fraction operator+ (fraction v) { return fraction(n * v.d + d * v.n, d * v.d); }
-  fraction operator+= (fraction v) { return *this = *this + v; }
-  fraction operator- (fraction v) { return fraction(n * v.d - d * v.n, d * v.d); }
-  fraction operator-= (fraction v) { return *this = *this - v; }
-  fraction operator* (fraction v) { return fraction(n * v.n, d * v.d); }
-  fraction operator*= (fraction v) { return *this = *this * v; }
-  fraction operator/ (fraction v) { return fraction(n * v.d, d * v.n); }
-  fraction operator/= (fraction v) { return *this = *this / v; }
-  fraction operator- () { return fraction(-n, d); }
-
-  strong_ordering operator<=> (fraction v) { return n * v.d <=> d * v.n; }
+  auto operator<=> (fraction v) { return n * v.d <=> d * v.n; }
 
   explicit operator bool() { return n != 0; }
   explicit operator double() { return (double)n / d; }
@@ -27,7 +21,7 @@ public:
     v.d = 1;
     return cin >> v.n;
   }
-  friend ostream& operator<< (ostream& cout, fraction& v) { return cout << v.n << '/'<< v.d; }
+  friend ostream& operator<< (ostream& cout, fraction& v) { return cout << v.n << '/' << v.d; }
 
   friend ll floor(fraction v) { return v.n / v.d - ((v.n % v.d != 0) && ((v.n < 0) != (v.d < 0))); }
   friend ll ceil(fraction v) { return v.n / v.d + ((v.n % v.d != 0) && ((v.n > 0) == (v.d > 0))); }
