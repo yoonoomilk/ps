93d647c424768b4348a9d892c6b7705e3751be70
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-11-08 23:23:10 +0900

    modint shorter

--- template/math/modint.cpp
+++ template/math/modint.cpp
@@ -6,13 +6,13 @@ public:
   _modint(ll n = 0) : val((n %= mod) < 0 ? n + mod : n) {}
   _modint(_modint v) : val(v.val) {}
   _modint operator+ (const _modint v) const { return _modint(val + v.val); }
-  _modint operator+= (const _modint v) { return *this = _modint(val + v.val); }
+  _modint operator+= (const _modint v) { return *this = *this + v; }
   _modint operator- (const _modint v) const { return _modint(val - v.val); }
-  _modint operator-= (const _modint v) { return *this = _modint(val - v.val); }
+  _modint operator-= (const _modint v) { return *this = *this - v; }
   _modint operator* (const _modint v) const { return _modint(val * v.val); }
-  _modint operator*= (const _modint v) { return *this = _modint(val * v.val); }
+  _modint operator*= (const _modint v) { return *this = *this * v; }
   _modint operator/ (const _modint v) const { return _modint(val * _pow<mod>(v.val, mod - 2)); }
-  _modint operator/= (const _modint v) { return *this = _modint(val * _pow<mod>(v.val, mod - 2)); }
+  _modint operator/= (const _modint v) { return *this = *this / v; }
   _modint operator- () { return _modint(-val); }
 
   friend istream& operator>> (istream& cin, _modint& v) {
