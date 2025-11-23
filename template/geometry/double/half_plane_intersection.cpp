bbbec9f42a494ee03c54ab19ffd8fdd1e3cf1886
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-11-23 21:17:19 +0900

    hpi

--- template/geometry/double/half_plane_intersection.cpp
+++ template/geometry/double/half_plane_intersection.cpp
@@ -2,28 +2,23 @@ polygon half_plane_intersection(vector<line>& lines) {
   auto bad = [](line& a, line& b, line& c) {
     point v;
     if(!line_intersection(a, b, v)) return false;
-    double d = (c.t - c.s)/ (v - c.s);
+    double d = (c.t - c.s) / (v - c.s);
     return d < 1e-9;
   };
   sort(lines.begin(), lines.end());
-  vector<line> unique_lines;
-  for(line i : lines) {
-    if(unique_lines.size()) {
-      line& prev = unique_lines.back();
-      double d = (prev.t - prev.s) / (i.t - i.s);
-      if(abs(d) < 1e-9) {
-        if((prev.t - prev.s) / (i.s - prev.s) > 0) prev = i;
-        continue;
-      }
-    }
-    unique_lines.push_back(i);
+  vector<line> unique_lines{lines[0]};
+  for(int i = 1;i < lines.size();i++) {
+    line& bef = unique_lines.back(), cur = lines[i];;
+    double d = (bef.t - bef.s) / (cur.t - cur.s);
+    if(abs(d) > 1e-9) unique_lines.push_back(cur);
+    else if(ccw(bef.s, bef.t, cur.s) > 0) bef = cur;
   }
-  lines = unique_lines;
+  swap(lines, unique_lines);
   deque<line> dq;
-  for(int i = 0;i < lines.size();i++) {
-    while(dq.size() >= 2 && bad(dq[dq.size() - 2], dq.back(), lines[i])) dq.pop_back();
-    while(dq.size() >= 2 && bad(dq[0], dq[1], lines[i])) dq.pop_front();
-    if(dq.size() < 2 || !bad(dq.back(), lines[i], dq[0])) dq.push_back(lines[i]);
+  for(line& i : lines) {
+    while(dq.size() >= 2 && bad(dq[dq.size() - 2], dq.back(), i)) dq.pop_back();
+    while(dq.size() >= 2 && bad(dq[0], dq[1], i)) dq.pop_front();
+    if(dq.size() < 2 || !bad(dq.back(), i, dq[0])) dq.push_back(i);
   }
   polygon tmp;
   if(dq.size() >= 3) for(int i = 0;i < dq.size();i++) {
--- template/short_things/pbds.md
+++ template/short_things/pbds.md
@@ -1,12 +1,24 @@
 ## ordered_set
 ```cpp
-#include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
 using namespace __gnu_pbds;
 
 template <typename T>
 using ordered_set = tree<T, null_type, less<T>, splay_tree_tag, tree_order_statistics_node_update>;
 ```
 multiset으로 쓰려면 pair에 넣기
+
+## faster unordered_set
+```cpp
+#include <ext/pb_ds/assoc_container.hpp>
+template <typename Key, typename T, typename Hash = hash<Key>>
+using hash_set = gp_hash_table<Key, T, Hash>;
+```
+### custom hash function
+```cpp
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct _hash {
+  size_t operator() (T x) const { return x ^ RANDOM; }
+};
+```
