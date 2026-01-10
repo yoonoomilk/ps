8fc8344a2e6760077ec5abd537978595dad360a2
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2026-01-10 21:25:31 +0900

    egcd

---
+++ snippets/math/egcd.cpp
@@ -0,0 +1,15 @@
+auto egcd(ll a, ll b) {
+  auto cur = tuple{a, 1LL, 0LL}, nxt = tuple{b, 0LL, 1LL};
+  while(get<0>(nxt)) {
+    ll d = get<0>(cur) / get<0>(nxt);
+    tie(cur, nxt) = tuple{
+      nxt,
+      tuple{
+        get<0>(cur) - d * get<0>(nxt),
+        get<1>(cur) - d * get<1>(nxt),
+        get<2>(cur) - d * get<2>(nxt)
+      }
+    };
+  }
+  return cur;
+}
